216cba8cf993d3663966d0755fe8e2cb6010fe05
Michał Oleksy <michaloleksy97@gmail.com>
2021-12-02 18:36:38 +0100

    Setup uvm dependencies

--- lab05/tb.f
+++ lab05/tb.f
@@ -2,4 +2,13 @@
 alu_pkg.sv
 alu_bfm.sv
 +incdir+.
++incdir+tb_classes
 top.sv
+
+-uvm
+-uvmhome /cad/XCELIUM1909/tools/methodology/UVM/CDNS-1.2/sv
++UVM_NO_RELNOTES
++UVM_VERBOSITY=MEDIUM
+-linedebug
+-fsmdebug
+-uvmlinedebug
