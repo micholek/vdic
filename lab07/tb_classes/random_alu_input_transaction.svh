06c8a78b194263db2b1c56f4106900870f854aff
Michał Oleksy <michaloleksy97@gmail.com>
2021-12-16 17:39:56 +0100

    Utilize post_randomize function

--- lab07/tb_classes/random_alu_input_transaction.svh
+++ lab07/tb_classes/random_alu_input_transaction.svh
@@ -50,6 +50,45 @@ class random_alu_input_transaction extends uvm_transaction;
             alu_input.crc, alu_input.action);
     endfunction : convert2string
 
+    function void post_randomize();
+        alu_input.crc = generate_in_crc(alu_input);
+    endfunction : post_randomize
+
+    protected function in_crc_t generate_in_crc(alu_input_t alu_input);
+        bit [31:0] X = alu_input.A;
+        bit [31:0] Y = alu_input.B;
+        bit [2:0] operation = alu_input.operation;
+        bit [67:0] d = {X, Y, 1'b1, operation};
+        static in_crc_t c = 0;
+        in_crc_t crc = {
+            d[67] ^ d[65] ^ d[63] ^ d[62] ^ d[59] ^ d[55] ^ d[54] ^ d[53] ^ d[52] ^ d[50] ^ d[48] ^
+            d[47] ^ d[44] ^ d[40] ^ d[39] ^ d[38] ^ d[37] ^ d[35] ^ d[33] ^ d[32] ^ d[29] ^ d[25] ^
+            d[24] ^ d[23] ^ d[22] ^ d[20] ^ d[18] ^ d[17] ^ d[14] ^ d[10] ^ d[9] ^ d[8] ^ d[7] ^
+            d[5] ^ d[3] ^ d[2] ^ c[1] ^ c[3],
+            d[67] ^ d[66] ^ d[64] ^ d[62] ^ d[61] ^ d[58] ^ d[54] ^ d[53] ^ d[52] ^ d[51] ^ d[49] ^
+            d[47] ^ d[46] ^ d[43] ^ d[39] ^ d[38] ^ d[37] ^ d[36] ^ d[34] ^ d[32] ^ d[31] ^ d[28] ^
+            d[24] ^ d[23] ^ d[22] ^ d[21] ^ d[19] ^ d[17] ^ d[16] ^ d[13] ^ d[9] ^ d[8] ^ d[7] ^
+            d[6] ^ d[4] ^ d[2] ^ d[1] ^ c[0] ^ c[2] ^ c[3],
+            d[67] ^ d[66] ^ d[65] ^ d[63] ^ d[61] ^ d[60] ^ d[57] ^ d[53] ^ d[52] ^ d[51] ^ d[50] ^
+            d[48] ^ d[46] ^ d[45] ^ d[42] ^ d[38] ^ d[37] ^ d[36] ^ d[35] ^ d[33] ^ d[31] ^ d[30] ^
+            d[27] ^ d[23] ^ d[22] ^ d[21] ^ d[20] ^ d[18] ^ d[16] ^ d[15] ^ d[12] ^ d[8] ^ d[7] ^
+            d[6] ^ d[5] ^ d[3] ^ d[1] ^ d[0] ^ c[1] ^ c[2] ^ c[3],
+            d[66] ^ d[64] ^ d[63] ^ d[60] ^ d[56] ^ d[55] ^ d[54] ^ d[53] ^ d[51] ^ d[49] ^ d[48] ^
+            d[45] ^ d[41] ^ d[40] ^ d[39] ^ d[38] ^ d[36] ^ d[34] ^ d[33] ^ d[30] ^ d[26] ^ d[25] ^
+            d[24] ^ d[23] ^ d[21] ^ d[19] ^ d[18] ^ d[15] ^ d[11] ^ d[10] ^ d[9] ^ d[8] ^ d[6] ^
+            d[4] ^ d[3] ^ d[0] ^ c[0] ^ c[2]
+        };
+        if (alu_input.invalid_crc) begin
+            in_crc_t invalid_crc;
+            do begin
+                invalid_crc = in_crc_t'($random);
+            end while (invalid_crc === crc);
+            return invalid_crc;
+        end else begin
+            return crc;
+        end
+    endfunction : generate_in_crc
+
     function new (string name = "");
         super.new(name);
     endfunction : new
